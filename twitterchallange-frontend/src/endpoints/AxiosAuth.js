681c1f3c3479ce54948867a16ff07320f46d0059
serkantoraman <serkan@github.com>
2023-07-20 17:00:41 +0300

    URL updates

--- twitterchallange-frontend/src/endpoints/AxiosAuth.js
+++ twitterchallange-frontend/src/endpoints/AxiosAuth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosWithAuth=()=>{
   const user = localStorage.getItem('user');
   return axios.create({
-    baseURL: "https://serkantoraman-twitterproject.onrender.com",
+    baseURL:"https://serkantoraman-twitterproject.onrender.com",
     headers: {
       Authorization:JSON.parse(user).token
     }
